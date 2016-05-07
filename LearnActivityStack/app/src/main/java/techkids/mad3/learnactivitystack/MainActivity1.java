0fd0468028613721eba324fdd7289fb0139648b7
Trần Trung <nguyentrantrung1986@gmail.com>
2016-05-08 01:36:00 +0700

    Learn Activity Stack

---
+++ LearnActivityStack/app/src/main/java/techkids/mad3/learnactivitystack/MainActivity1.java
@@ -0,0 +1,13 @@
+package techkids.mad3.learnactivitystack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+}
