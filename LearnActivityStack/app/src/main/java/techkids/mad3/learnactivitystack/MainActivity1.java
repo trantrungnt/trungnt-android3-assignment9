dd7bff24a46c652a8aba799a48ed621d604ce5bf
Trần Trung <nguyentrantrung1986@gmail.com>
2016-05-08 09:34:44 +0700

    layout acitivity from 1 to 10 (temp)

--- LearnActivityStack/app/src/main/java/techkids/mad3/learnactivitystack/MainActivity1.java
+++ LearnActivityStack/app/src/main/java/techkids/mad3/learnactivitystack/MainActivity1.java
@@ -1,5 +1,6 @@
 package techkids.mad3.learnactivitystack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,8 @@ public class MainActivity1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main1);
+        setContentView(R.layout.activity_layout_1);
+
+        
     }
 }
